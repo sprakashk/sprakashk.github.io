576a60dea1076eb2479ca05ab048b3eeb50c427a
Satyaprakash Kumawat <satyaprakash.kumawat@paytm.com>
2021-01-31 16:18:40 +0530

    Build

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/satyaprakash/Documents/learn/sprakashk.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/logo.svg","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"0cfc31aa90b4b17508de2bb712f64b12"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
